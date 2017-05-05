1763ddc7db77f9a374736f3b9fc8f857ca12216e
Fabien LOISON <flo@flogisoft.com>
2017-05-05 16:36:22 +0200

    high level function implemented

--- test/test_pyguetzli.py
+++ test/test_pyguetzli.py
@@ -31,3 +31,14 @@ def test_optimize_from_image_bytes_jpeg():
     image2 = pyguetzli.optimize_from_image_bytes(data, 80)
     assert image2.length < image.length
 
+
+def test_optimize_from_rgb_array():
+    data = image_pixels = bytes(bytearray([
+        0xFF, 0x00, 0x00,   0x00, 0xFF, 0x00,
+        0x00, 0x00, 0xFF,   0xFF, 0xFF, 0xFF,
+        ]))
+    image = pyguetzli.optimize_from_rgb_bytes(data, 2, 2, 100)
+    assert type(image) is pyguetzli.guetzli.GuetzliImage
+    assert image.length > 0
+    assert image.to_bytes().startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
+
