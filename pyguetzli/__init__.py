9e89316f4f8433f2f4284e7fc4380949f6d9edc0
Fabien LOISON <flo@flogisoft.com>
2017-07-10 17:53:23 +0200

    wip main api + tests

--- pyguetzli/__init__.py
+++ pyguetzli/__init__.py
@@ -1,20 +1,10 @@
-from . import guetzli
+from .guetzli import (
+        DEFAULT_JPEG_QUALITY,
+        process_jpeg_bytes,
+        process_rgb_bytes,
+        )
 
 
-def process_image_from_file(path, quality=guetzli.DEFAULT_JPEG_QUALITY):
-    image = guetzli.guetzli_image_read_file(path)
-    image_opti = guetzli.guetzli_image_process(image, quality)
-    return image_opti
 
 
-def process_image_from_bytes(data, quality=guetzli.DEFAULT_JPEG_QUALITY):
-    image = guetzli.GuetzliImage(data, guetzli.GuetzliImage.TYPE_JPEG)  # TODO detect format from bytes
-    image_opti = guetzli.guetzli_image_process(image, quality)
-    return image_opti
-
-
-def process_rgb_bytes(data, width, height, quality=guetzli.DEFAULT_JPEG_QUALITY):
-    rgb_array = guetzli.GuetzliRgbArray(data, width, height)
-    image_opti = guetzli.guetzli_rgb_array_process(rgb_array, quality)
-    return image_opti
 
