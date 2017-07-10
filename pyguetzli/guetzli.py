7e37db9ad5316e303b12e37562467aeb2244e232
Fabien LOISON <flo@flogisoft.com>
2017-07-10 16:14:47 +0200

    guetzli: new API and tests for new API

--- pyguetzli/guetzli.py
+++ pyguetzli/guetzli.py
@@ -1,102 +1,31 @@
-import platform
-
 from ._guetzli import lib, ffi
 
 
 DEFAULT_JPEG_QUALITY = 95
 
 
-def _str_to_bytes(str):
-    if platform.python_version_tuple()[0] == "3":
-        return bytes(str, "utf-8")
-    return str
+def process_jpeg_bytes(bytes_in, quality=DEFAULT_JPEG_QUALITY):
+    """Generates an optimized JPEG from JPEG-encoded bytes.
+
+    Arguments:
+    bytes_in -- the input image's bytes
+
+    Keyword Arguments:
+    quality -- the output JPEG quality (default 95)
+    """
+    pass
 
 
-class GuetzliImage(object):
+def process_rgb_bytes(bytes_in, width, height, quality=DEFAULT_JPEG_QUALITY):
+    """Generates an optimized JPEG from RGB bytes.
 
-    TYPE_JPEG = lib.GUETZLI_IMAGE_TYPE_JPEG
-    TYPE_PNG = lib.GUETZLI_IMAGE_TYPE_PNG
-    TYPE_UNKNOWN = lib.GUETZLI_IMAGE_TYPE_UNKNOWN
+    Arguments:
+    bytes_in -- the input image's bytes
+    width -- the width of the input image
+    height -- the height of the input image
 
-    _cdata = None
-
-    def __init__(self, data, type_):
-        guetzli_image_p = lib.guetzliImageNew(type_, len(data))
-        ffi.memmove(guetzli_image_p.data, data, len(data))
-        guetzli_image_p_gc = ffi.gc(guetzli_image_p, lib.guetzliImageFree)
-        self._cdata = guetzli_image_p_gc
-
-    @classmethod
-    def from_guetzli_image_p(cls, guetzli_image_p):
-        if ffi.typeof(guetzli_image_p).cname != "GuetzliImage *":
-            raise ValueError("Expected guetzli_image_p to be a 'GuetzliImage *'")
-        image = cls.__new__(cls)
-        image._cdata = guetzli_image_p
-        return image
-
-    @property
-    def length(self):
-        return self._cdata.length
-
-    @property
-    def type(self):
-        return self._cdata.type
-
-    def to_bytes(self):
-        return ffi.unpack(self._cdata.data, self._cdata.length)
-
-    def save(self, path):
-        lib.guetzliImageWriteFile(_str_to_bytes(path), self._cdata)
-
-
-class GuetzliRgbArray(object):
-
-    _cdata = None
-
-    def __init__(self, data, width, height):
-        data_length = width * height * 3
-        if len(data) != data_length:
-            raise ValueError("%ix%ipx image, but %i Bytes given. Does the given array contains only rgb data?")
-        guetzli_rgb_array_p = lib.guetzliRgbArrayNew(width, height)
-        ffi.memmove(guetzli_rgb_array_p.data, data, data_length)
-        guetzli_rgb_array_p_gc = ffi.gc(guetzli_rgb_array_p, lib.guetzliRgbArrayFree)
-        self._cdata = guetzli_rgb_array_p_gc
-
-    @property
-    def width(self):
-        return self._cdata.width
-
-    @property
-    def height(self):
-        return self._cdata.height
-
-    @property
-    def length(self):
-        return self.width * self.height * 3
-
-    def to_bytes(self):
-        return ffi.unpack(self._cdata.data, self.length)
-
-
-def guetzli_image_read_file(path):
-    guetzli_image_p = lib.guetzliImageReadFile(_str_to_bytes(path))
-
-    if guetzli_image_p == ffi.NULL:
-        raise IOError("Could not open the file")
-
-    guetzli_image_p_gc = ffi.gc(guetzli_image_p, lib.guetzliImageFree)
-
-    return GuetzliImage.from_guetzli_image_p(guetzli_image_p_gc)
-
-
-def guetzli_image_process(image, quality=DEFAULT_JPEG_QUALITY):
-    opti_guetzli_image_p = lib.guetzliImageProcess(image._cdata, quality)
-    opti_guetzli_image_p_gc = ffi.gc(opti_guetzli_image_p, lib.guetzliImageFree)
-    return GuetzliImage.from_guetzli_image_p(opti_guetzli_image_p_gc)
-
-
-def guetzli_rgb_array_process(rgb_array, quality=DEFAULT_JPEG_QUALITY):
-    opti_guetzli_image_p = lib.guetzliRgbArrayProcess(rgb_array._cdata, quality)
-    opti_guetzli_image_p_gc = ffi.gc(opti_guetzli_image_p, lib.guetzliImageFree)
-    return GuetzliImage.from_guetzli_image_p(opti_guetzli_image_p_gc)
+    Keyword Arguments:
+    quality -- the output JPEG quality (default 95)
+    """
+    pass
 
--- test/test_guetzli.py
+++ test/test_guetzli.py
@@ -3,82 +3,50 @@ import pytest
 from pyguetzli import guetzli
 
 
-JPEG_PATH = "test/image.jpg"
-JPEG_LENGTH = 152485
-JPEG_WIDTH = 256
-JPEG_HEIGHT = 256
-JPEG_PIXEL_COUNT = JPEG_WIDTH * JPEG_HEIGHT
-
-
-class TestGuetzliImage(object):
+class Test_process_jpeg_bytes(object):
 
     def setup(self):
-        self.img = guetzli.GuetzliImage(b"\x00\x01\x02\x03", guetzli.GuetzliImage.TYPE_UNKNOWN)
+        self.jpeg_bytes = open("./test/image.jpg", "rb").read()
+        self.misc_bytes = b"\x01\x02\x03\x04"
 
-    def test_length(self):
-        assert self.img.length == 4
+    def test_jpeg_image(self):
+        optimized_jpeg_bytes = guetzli.process_jpeg_bytes(self.jpeg_bytes)
+        assert type(optimized_jpeg_bytes) == type(b"")
+        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
+        assert len(optimized_jpeg_bytes) < len(self.jpeg_bytes)
 
-    def test_type(self):
-        assert self.img.type == guetzli.GuetzliImage.TYPE_UNKNOWN
+    def test_quality_param(self):
+        optimized_jpeg_bytes_q100 = guetzli.process_jpeg_bytes(self.jpeg_bytes, 100)
+        optimized_jpeg_bytes_q50 = guetzli.process_jpeg_bytes(self.jpeg_bytes, 50)
+        assert len(optimized_jpeg_bytes_q50) < len(optimized_jpeg_bytes_q100)
 
-    def test_to_bytes(self):
-        assert self.img.to_bytes() == b"\x00\x01\x02\x03"
-
-
-class TestGuetzliImageReadFile(object):
-
-    def test_none_existing_file(self):
-        with pytest.raises(IOError):
-            image = guetzli.guetzli_image_read_file("this-file-do-not-exists")
-
-    def test_jpeg_file(self):
-        image = guetzli.guetzli_image_read_file(JPEG_PATH)
-        assert type(image) is guetzli.GuetzliImage
-        assert image.length == JPEG_LENGTH
-        assert image.to_bytes().startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-
-
-class TestGuetzliImageProcess(object):
-
-    def test_jpeg_guetzli_image(self):
-        image = guetzli.guetzli_image_read_file(JPEG_PATH)
-        image_opti = guetzli.guetzli_image_process(image, 100)
-        assert type(image) is guetzli.GuetzliImage
-        assert image_opti.length < image.length
-        assert image_opti.to_bytes().startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-
-
-class TestGuetzliRgbArray(object):
-
-    def setup(self):
-        self.rgb = guetzli.GuetzliRgbArray(b"\x00\x01\x02\x0a\x0b\x0c", 1, 2)
-
-    def test_uncoherent_values(self):
+    def test_non_jpeg_image(self):
         with pytest.raises(ValueError):
-            rgb_array = guetzli.GuetzliRgbArray("0123", 10, 10)
+            guetzli.process_jpeg_bytes(self.misc_bytes)
 
-    def test_width(self):
-        assert self.rgb.width == 1
-
-    def test_height(self):
-        assert self.rgb.height == 2
-
-    def test_length(self):
-        assert self.rgb.length == 6
-
-    def test_to_bytes(self):
-        assert self.rgb.to_bytes() == b"\x00\x01\x02\x0a\x0b\x0c"
+    def test_truncated_jpeg(self):
+        with pytest.raises(ValueError):
+            guetzli.process_jpeg_bytes(self.jpeg_bytes[:64])
 
 
-class TestGuetzliRgbArrayProcess(object):
+class Test_process_rgb_bytes(object):
 
-    def test_4px_iamge(self):
-        image_pixels = bytes(bytearray([
+    def setup(self):
+        self.rgb_bytes = bytes(bytearray([
             0xFF, 0x00, 0x00,   0x00, 0xFF, 0x00,
             0x00, 0x00, 0xFF,   0xFF, 0xFF, 0xFF,
             ]))
-        rgb_array = guetzli.GuetzliRgbArray(image_pixels, 2, 2)
-        image = guetzli.guetzli_rgb_array_process(rgb_array)
-        assert type(image) is guetzli.GuetzliImage
-        assert image.length > 0
-        assert image.to_bytes().startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
+
+    def test_rgb_data(self):
+        optimized_jpeg_bytes = guetzli.process_rgb_bytes(self.rgb_bytes, 2, 2)
+        assert type(optimized_jpeg_bytes) == type(b"")
+        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
+
+    def test_quality_param(self):
+        optimized_jpeg_bytes_q100 = guetzli.process_rgb_bytes(self.rgb_bytes, 2, 2, 100)
+        optimized_jpeg_bytes_q50 = guetzli.process_rgb_bytes(self.rgb_bytes, 2, 2, 50)
+        assert len(optimized_jpeg_bytes_q50) < len(optimized_jpeg_bytes_q100)
+
+    def test_wrong_width_height(self):
+        with pytest.raises(ValueError):
+            optimized_jpeg_bytes = guetzli.process_rgb_bytes(self.rgb_bytes, 4, 4)
