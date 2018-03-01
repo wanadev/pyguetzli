a37efd080ebc5a7e99a2966c36704c9100446b54
Fabien LOISON <flo@flogisoft.com>
2018-03-01 10:27:07 +0100

    Improves tests

--- test/test_guetzli.py
+++ test/test_guetzli.py
@@ -3,55 +3,64 @@ import pytest
 from pyguetzli import guetzli
 
 
-class Test_process_jpeg_bytes(object):
-
-    def setup(self):
-        self.jpeg_bytes = open("./test/image.jpg", "rb").read()
-        self.misc_bytes = b"\x01\x02\x03\x04"
-
-    def test_jpeg_image(self):
-        optimized_jpeg_bytes = guetzli.process_jpeg_bytes(self.jpeg_bytes)
-        assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-        assert len(optimized_jpeg_bytes) < len(self.jpeg_bytes)
-
-    def test_quality_param(self):
-        optimized_jpeg_bytes_q100 = guetzli.process_jpeg_bytes(
-                self.jpeg_bytes, 100)
-        optimized_jpeg_bytes_q50 = guetzli.process_jpeg_bytes(
-                self.jpeg_bytes, 50)
-        assert len(optimized_jpeg_bytes_q50) < len(optimized_jpeg_bytes_q100)
-
-    def test_non_jpeg_image(self):
-        with pytest.raises(ValueError):
-            guetzli.process_jpeg_bytes(self.misc_bytes)
-
-    def test_truncated_jpeg(self):
-        with pytest.raises(ValueError):
-            guetzli.process_jpeg_bytes(self.jpeg_bytes[:64])
+JPEG_MAGIC = b"\xFF\xD8\xFF\xE0\x00\x10JFIF"
 
 
-class Test_process_rgb_bytes(object):
+@pytest.fixture
+def jpeg_bytes():
+    return open("./test/image.jpg", "rb").read()
 
-    def setup(self):
-        self.rgb_bytes = bytes(bytearray([
+
+@pytest.fixture
+def misc_bytes():
+    return b"\x01\x02\x03\x04"
+
+
+@pytest.fixture
+def rgb_bytes():
+    return bytes(bytearray([
             0xFF, 0x00, 0x00,   0x00, 0xFF, 0x00,
             0x00, 0x00, 0xFF,   0xFF, 0xFF, 0xFF,
             ]))
 
-    def test_rgb_data(self):
-        optimized_jpeg_bytes = guetzli.process_rgb_bytes(self.rgb_bytes, 2, 2)
-        assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
 
-    @pytest.mark.skip(reason="we need a larger image to be able to test this")
-    def test_quality_param(self):
-        optimized_jpeg_bytes_q100 = guetzli.process_rgb_bytes(
-                self.rgb_bytes, 2, 2, 100)
-        optimized_jpeg_bytes_q50 = guetzli.process_rgb_bytes(
-                self.rgb_bytes, 2, 2, 50)
+class Test_process_jpeg_bytes(object):
+
+    def test_jpeg_image(self, jpeg_bytes):
+        optimized_jpeg_bytes = guetzli.process_jpeg_bytes(jpeg_bytes)
+        assert isinstance(optimized_jpeg_bytes, bytes)
+        assert optimized_jpeg_bytes.startswith(JPEG_MAGIC)
+        assert len(optimized_jpeg_bytes) < len(jpeg_bytes)
+
+    def test_quality_param(self, jpeg_bytes):
+        optimized_jpeg_bytes_q100 = guetzli.process_jpeg_bytes(jpeg_bytes, 100)
+        optimized_jpeg_bytes_q50 = guetzli.process_jpeg_bytes(jpeg_bytes, 50)
         assert len(optimized_jpeg_bytes_q50) < len(optimized_jpeg_bytes_q100)
 
-    def test_wrong_width_height(self):
+    def test_non_jpeg_image(self, misc_bytes):
         with pytest.raises(ValueError):
-            guetzli.process_rgb_bytes(self.rgb_bytes, 4, 4)
+            guetzli.process_jpeg_bytes(misc_bytes)
+
+    def test_truncated_jpeg(self, jpeg_bytes):
+        with pytest.raises(ValueError):
+            guetzli.process_jpeg_bytes(jpeg_bytes[:64])
+
+
+class Test_process_rgb_bytes(object):
+
+    def test_rgb_data(self, rgb_bytes):
+        optimized_jpeg_bytes = guetzli.process_rgb_bytes(rgb_bytes, 2, 2)
+        assert isinstance(optimized_jpeg_bytes, bytes)
+        assert optimized_jpeg_bytes.startswith(JPEG_MAGIC)
+
+    @pytest.mark.skip(reason="we need a larger image to be able to test this")
+    def test_quality_param(self, rgb_bytes):
+        optimized_jpeg_bytes_q100 = guetzli.process_rgb_bytes(
+                rgb_bytes, 2, 2, 100)
+        optimized_jpeg_bytes_q50 = guetzli.process_rgb_bytes(
+                rgb_bytes, 2, 2, 50)
+        assert len(optimized_jpeg_bytes_q50) < len(optimized_jpeg_bytes_q100)
+
+    def test_wrong_width_height(self, rgb_bytes):
+        with pytest.raises(ValueError):
+            guetzli.process_rgb_bytes(rgb_bytes, 4, 4)
--- test/test_pil_image.py
+++ test/test_pil_image.py
@@ -1,45 +1,32 @@
+import pytest
 from PIL import Image
 
 from pyguetzli import pil_image
 
 
+JPEG_MAGIC = b"\xFF\xD8\xFF\xE0\x00\x10JFIF"
+
+
 class Test_process_pil_image(object):
 
-    def setup(self):
-        self.pil_image = Image.open("./test/image.jpg")
-
-    def test_pil_image(self):
-        optimized_jpeg_bytes = pil_image.process_pil_image(self.pil_image)
-        assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-
-    def test_pil_image_from_rgb_png(self):
-        image = Image.open("./test/rgb.png")
+    @pytest.mark.parametrize("image_path", [
+            "./test/image.jpg",
+            "./test/rgb.png",
+            "./test/rgba.png",
+            "./test/grayscale.png",
+            "./test/indexed.png",
+        ])
+    def test_pil_image(self, image_path):
+        image = Image.open(image_path)
         optimized_jpeg_bytes = pil_image.process_pil_image(image)
         assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-
-    def test_pil_image_from_rgba_png(self):
-        image = Image.open("./test/rgba.png")
-        optimized_jpeg_bytes = pil_image.process_pil_image(image)
-        assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-
-    def test_pil_image_from_grayscale_png(self):
-        image = Image.open("./test/grayscale.png")
-        optimized_jpeg_bytes = pil_image.process_pil_image(image)
-        assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
-
-    def test_pil_image_from_indexed_png(self):
-        image = Image.open("./test/indexed.png")
-        optimized_jpeg_bytes = pil_image.process_pil_image(image)
-        assert isinstance(optimized_jpeg_bytes, bytes)
-        assert optimized_jpeg_bytes.startswith(b"\xFF\xD8\xFF\xE0\x00\x10JFIF")
+        assert optimized_jpeg_bytes.startswith(JPEG_MAGIC)
 
     def test_quality_param(self):
+        image = Image.open("./test/image.jpg")
+
         optimized_jpeg_bytes_q100 = pil_image.process_pil_image(
-                self.pil_image, 100)
+                image, 100)
         optimized_jpeg_bytes_q50 = pil_image.process_pil_image(
-                self.pil_image, 50)
+                image, 50)
         assert len(optimized_jpeg_bytes_q50) < len(optimized_jpeg_bytes_q100)
