27a9c8d3efdfbe754ab077a6e61629d6a6e010b3
Fabien LOISON <flo@flogisoft.com>
2021-08-31 16:53:38 +0200

    Use setuptools instead of distutils to manage the ext build (distutils will be deprecated in Python 3.10 and removed in Python 3.12)

--- setup.py
+++ setup.py
@@ -3,55 +3,58 @@
 
 import io
 import os
-from distutils import ccompiler
 
 from setuptools import setup, find_packages
-from setuptools.command.build_py import build_py
+from setuptools.command.build_ext import build_ext
 
 
-class CustomBuildPy(build_py):
-
-    def run(self):
+class CustomBuildExt(build_ext):
+    def build_extensions(self):
         extra_cc_args = []
 
-        if ccompiler.get_default_compiler() == "unix":
+        if self.compiler.compiler_type == "unix":
             os.environ["CPPFLAGS"] = "--std=c++11"
             extra_cc_args = ["-fPIC", "--std=c++11", "-O3"]
 
-        if ccompiler.get_default_compiler() == "msvc":
+        if self.compiler.compiler_type == "msvc":
             extra_cc_args = ["/O2"]
 
-        compiler = ccompiler.new_compiler()
-        compiler.set_include_dirs([
-            "guetzli/",
-            "guetzli/third_party/butteraugli/",
-            ])
-        objects = compiler.compile([
-            "guetzli/guetzli/butteraugli_comparator.cc",
-            "guetzli/guetzli/dct_double.cc",
-            "guetzli/guetzli/debug_print.cc",
-            "guetzli/guetzli/entropy_encode.cc",
-            "guetzli/guetzli/fdct.cc",
-            "guetzli/guetzli/gamma_correct.cc",
-            "guetzli/guetzli/idct.cc",
-            "guetzli/guetzli/jpeg_data.cc",
-            "guetzli/guetzli/jpeg_data_decoder.cc",
-            "guetzli/guetzli/jpeg_data_encoder.cc",
-            "guetzli/guetzli/jpeg_data_reader.cc",
-            "guetzli/guetzli/jpeg_data_writer.cc",
-            "guetzli/guetzli/jpeg_huffman_decode.cc",
-            "guetzli/guetzli/output_image.cc",
-            "guetzli/guetzli/preprocess_downsample.cc",
-            "guetzli/guetzli/processor.cc",
-            "guetzli/guetzli/quality.cc",
-            "guetzli/guetzli/quantize.cc",
-            "guetzli/guetzli/score.cc",
-            "guetzli/third_party/butteraugli/butteraugli/butteraugli.cc",
+        self.compiler.set_include_dirs(
+            [
+                "guetzli/",
+                "guetzli/third_party/butteraugli/",
+            ]
+        )
+        objects = self.compiler.compile(
+            [
+                "guetzli/guetzli/butteraugli_comparator.cc",
+                "guetzli/guetzli/dct_double.cc",
+                "guetzli/guetzli/debug_print.cc",
+                "guetzli/guetzli/entropy_encode.cc",
+                "guetzli/guetzli/fdct.cc",
+                "guetzli/guetzli/gamma_correct.cc",
+                "guetzli/guetzli/idct.cc",
+                "guetzli/guetzli/jpeg_data.cc",
+                "guetzli/guetzli/jpeg_data_decoder.cc",
+                "guetzli/guetzli/jpeg_data_encoder.cc",
+                "guetzli/guetzli/jpeg_data_reader.cc",
+                "guetzli/guetzli/jpeg_data_writer.cc",
+                "guetzli/guetzli/jpeg_huffman_decode.cc",
+                "guetzli/guetzli/output_image.cc",
+                "guetzli/guetzli/preprocess_downsample.cc",
+                "guetzli/guetzli/processor.cc",
+                "guetzli/guetzli/quality.cc",
+                "guetzli/guetzli/quantize.cc",
+                "guetzli/guetzli/score.cc",
+                "guetzli/third_party/butteraugli/butteraugli/butteraugli.cc",
             ],
-            extra_preargs=extra_cc_args)
-        compiler.create_static_lib(objects, "guetzli", output_dir="guetzli")
+            extra_preargs=extra_cc_args,
+        )
+        self.compiler.create_static_lib(
+            objects, "guetzli", output_dir="guetzli"
+        )
 
-        build_py.run(self)
+        build_ext.build_extensions(self)
 
 
 long_description = ""
@@ -65,16 +68,12 @@ setup(
     description="Python bindings for Google's Guetzli, a JPEG encoder that optimises JPEG compression",  # noqa
     url="https://github.com/wanadev/pyguetzli",
     license="Apache-2.0",
-
     long_description=long_description,
     keywords="image jpeg optimize guetzli",
-
     author="Wanadev",
     author_email="contact@wanadev.fr",
     maintainer="Fabien LOISON, Alexis BREUST",
-
     packages=find_packages(),
-
     setup_requires=["cffi>=1.0.0"],
     install_requires=["cffi>=1.0.0"],
     extras_require={
@@ -87,11 +86,8 @@ setup(
             "pytest",
         ],
     },
-
     cffi_modules=["pyguetzli/guetzli_build.py:ffibuilder"],
-
     cmdclass={
-        "build_py": CustomBuildPy,
+        "build_ext": CustomBuildExt,
     },
-
 )
